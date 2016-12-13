b0ae417bfb7b4beedd218199870ef926bdeaeab5
Ryan Chin <ryanchin@me.com>
2016-12-12 21:10:25 -0500

    Add files via upload

--- fb.js
+++ fb.js
@@ -2,5 +2,5 @@
 module.exports = {
   'appID' : '1420584861581987',
   'appSecret' : 'd48dcf1b817c18fd7c8f25491e1066b6',
-  'callbackUrl' : 'http://ec2-54-86-69-175.compute-1.amazonaws.com/3000/login/callback'
+  'callbackUrl' : 'http://ec2-54-86-69-175.compute-1.amazonaws.com:3000/login/callback'
 }
